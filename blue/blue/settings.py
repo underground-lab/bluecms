8cb14ea450df7fd30b55e221b0665f5dbf436a83
Jaroslav Beran <jaroslav.beran@gmail.com>
2023-07-05 13:38:09 +0200

    Website description

--- blue/blue/settings.py
+++ blue/blue/settings.py
@@ -156,8 +156,11 @@ EMAIL_RECIPIENT = ''
 BLUECMS_TITLE1 = 'blue'
 BLUECMS_TITLE2 = 'cms'
 BLUECMS_SUBTITLE = 'Simple. Contemporary. CMS Template.'
+BLUECMS_DESCRIPTION = 'website description'
+BLUECMS_KEYWORDS = 'website keywords, website keywords'
 BLUECMS_FOOTER = "blueCopyright &copy; colour_blue | <a href='http://validator.w3.org/check?uri=referer'>HTML5</a> | <a href='http://jigsaw.w3.org/css-validator/check/referer'>CSS</a> | <a href='http://www.html5webtemplates.co.uk'>design from HTML5webtemplates.co.uk</a>"
 BLUECMS_GOOGLE_ANALYTICS = None
+
 
 # Load local settings if exists
 try:
